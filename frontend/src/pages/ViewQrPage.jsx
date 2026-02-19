675220e7890d5b0a0ab71b952ca759cb36f322c8
SubhamSahu809 <subhamkumarsahu896@gmail.com>
2026-02-19 23:53:21 +0530

    cors issue

--- frontend/src/pages/ViewQrPage.jsx
+++ frontend/src/pages/ViewQrPage.jsx
@@ -20,7 +20,7 @@ function ViewQrPage() {
 
     try {
       setLoading(true);
-      const res = await axios.get("http://localhost:5000/tokens/viewQr" || "https://or-token-management.onrender.com/tokens/viewQr", {
+      const res = await axios.get("https://or-token-management.onrender.com/tokens/viewQr", {
         params: { mobileNumber },
       });
       setResult(res.data);
